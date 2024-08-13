de0969bc46d503e91c8b11ac0ad678132b8941c7
Jeffrey Ojuederhie <vgauss07@gmail.com>
2024-08-12 20:54:37 -0400

    more exercises

--- list_comprehension/exercises.py
+++ list_comprehension/exercises.py
@@ -33,3 +33,30 @@ print(len(string))
 spaces = "Jeffrey you have the mind of Christ"
 num_of_space = [len(space) for space in spaces if space == " "]
 print(num_of_space)
+
+
+"""
+Given a list of numbers, remove all odd numbers from the list:
+"""
+numbers = [3,5,45,97,32,22,10,19,39,43]
+result = []
+odd_num = []
+
+# print out even numbers
+[result.append(num) for num in numbers if num % 2 == 0]
+
+[odd_num.append(num) for num in numbers if num % 2 != 0]
+print(odd_num)
+print(result)
+
+"""
+Create a list of all the consonants in the string “Yellow Yaks 
+like yelling and yawning and yesturday they yodled while eating yuky yams”
+"""
+vowels = "AEIOUaeiou"
+word = "Yellow Yaks like yelling and yawning and yesturday they yodled while eating yuky yams"
+consonants = []
+[consonants.append(letter) for letter in word if letter not in vowels]
+print(consonants)
+
+
